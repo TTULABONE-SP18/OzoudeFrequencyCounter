62fe6d8284108d4adb9602759a17f76466f75917
Victoria <rice.rodriguez@ttu.edu>
2018-01-29 12:43:56 -0600

    Fixed, added clk input

--- VerilogFiles/disp_freq.v
+++ VerilogFiles/disp_freq.v
@@ -1,29 +1,26 @@
 `timescale 1 ns / 1 ps
 // Frequency Counter Display Module
 module disp_freq(
+	input clk,
 	input [3:0] freq,
 	output reg [6:0] disp_out
 	);
 // the hex-to-7-segment decoder
-    always @ (freq)
+    always @ (posedge clk)
+    begin
         case (freq)
-        4'b0000: disp_out = 7'b1111110;  // 0
-        4'b0001: disp_out = 7'b0110000;  // 1
-        4'b0010: disp_out = 7'b1101101;  // 2
-        4'b0011: disp_out = 7'b1111001;  // 3
-        4'b0100: disp_out = 7'b0110011;  // 4
-        4'b0101: disp_out = 7'b1011011;  // 5
-        4'b0110: disp_out = 7'b1011111;  // 6
-        4'b0111: disp_out = 7'b1110000;  // 7
-        4'b1000: disp_out = 7'b1111111;  // 8
-        4'b1001: disp_out = 7'b1110011;  // 9
-        4'b1010: disp_out = 7'b1110111;  // A
-        4'b1011: disp_out = 7'b0011111;  // b
-        4'b1100: disp_out = 7'b0001101;  // c
-        4'b1101: disp_out = 7'b0111101;  // d
-        4'b1110: disp_out = 7'b1001111;  // E
-        4'b1111: disp_out = 7'b1000111;  // F
-        default: disp_out = 7'bxxxxxxx;
+	   4'b0000: LED_out = 7'b0000001; // "0"
+        4'b0001: LED_out = 7'b1001111; // "1"
+        4'b0010: LED_out = 7'b0010010; // "2"
+        4'b0011: LED_out = 7'b0000110; // "3"
+        4'b0100: LED_out = 7'b1001100; // "4"
+        4'b0101: LED_out = 7'b0100100; // "5"
+        4'b0110: LED_out = 7'b0100000; // "6"
+        4'b0111: LED_out = 7'b0001111; // "7"
+        4'b1000: LED_out = 7'b0000000; // "8"
+        4'b1001: LED_out = 7'b0000100; // "9"
+        default: LED_out = 7'b0000001; // "0"
         endcase
+	end
 
 endmodule
