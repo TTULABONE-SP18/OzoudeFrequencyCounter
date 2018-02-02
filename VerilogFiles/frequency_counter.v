cca3885682d7cdcee9621acaacee72106ebe766e
Victoria <rice.rodriguez@ttu.edu>
2018-02-02 11:00:04 -0600

    Formatting

--- VerilogFiles/frequency_counter.v
+++ VerilogFiles/frequency_counter.v
@@ -1,19 +1,19 @@
 `timescale 1 ns / 1 ps
 // Frequency Counter Module
-// Developer: 	 Victoria Rodriguez
+// Developer:   Victoria Rodriguez
 // Description: This module counts the number of inputs it receives in one second using the 100 MHz
-// 			 clock.
+//              clock.
 // Methodology: This module was coded for a 100 MHz clock, meaning that there should be 100 million
-//			 ticks in one second. That means that after the counter has counted 100 million ticks
-// 			 we know that one second has passed. Another counter works simultaneously to count
-// 			 the number of inputs coming in from the Pmod port. After one second has elapsed,
-// 			 then we know that the number of inputs (cycles) is the frequency of of the signal
-// 			 coming into the Pmod port. Hertz is measured in cycles/sec, and since we know it's
-// 			 been one second, we don't have to do any further math-- the input counter should be
-// 			 the frequency of the input as is.
+//              ticks in one second. That means that after the counter has counted 100 million ticks
+//              we know that one second has passed. Another counter works simultaneously to count
+//              the number of inputs coming in from the Pmod port. After one second has elapsed,
+//              we know that the number of inputs (cycles) is the frequency of of the signal
+//              into the Pmod port. Hertz is measured in cycles/sec, and since we know it's
+//              one second, we don't have to do any further math-- the input counter should be
+//              frequency of the input as is.
 
 module freq_counter(
-     // 100 MHz clock
+	// 100 MHz clock
 	input CLK,
 	// Pmod port input JA1, renamed in constraints file to IN
 	input [1:0] IN,
@@ -36,7 +36,7 @@ module freq_counter(
 	assign DP = 1;
 	localparam max = 100000000;
 
-     // Flip-flop stores last value in register
+	// Flip-flop stores last value in register
 	always @(posedge CLK) begin
 		last <= IN;
 	end
