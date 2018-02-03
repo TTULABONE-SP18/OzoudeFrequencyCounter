06b1e10b15a6966c825d6b6d0fb43c1a3e2dffcd
Victoria <rice.rodriguez@ttu.edu>
2018-02-03 15:20:51 -0600

    Added bitstream file

--- VerilogFiles/Testbenches/bcd_tb.v
+++ VerilogFiles/Testbenches/bcd_tb.v
@@ -3,10 +3,12 @@
 // Description:
 // Methodology:
 module bcd_tb();
-     wire [7:0] binary = 8'b10010001;   // 145 in decimal, 91 in hex
-     wire [3:0] hundreds = 4'b0,
-                tens     = 4'b0,
-                ones     = 4'b0;
+     wire [7:0] bin = 8'b10010001;   // 145 in decimal, 91 in hex
+     wire [3:0] tens     = 4'b0,
+                ones     = 4'b0,
+                hundreds = 4'b0;
+     wire [11:0] final   = 12'b0;
+     assign final = {hundreds, tens, ones};
      // reg clk;
      // initial begin
      //      clk = 0;
@@ -16,10 +18,12 @@ module bcd_tb();
 
      bin_bcd tb_bcd(
           // .clk(clk),
-          .bin(binary),
+          .binary(bin),
           .hundreds(hundreds),
           .tens(tens),
           .ones(ones)
           );
+
+//     reg [11:0] final = {hundreds,tens,ones};
 
 endmodule
